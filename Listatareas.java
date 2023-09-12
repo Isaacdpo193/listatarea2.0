d652f363887354329ff93d2ed92c6c04252d28dc
Isaacdpo193 <143726091+Isaacdpo193@users.noreply.github.com>
2023-09-11 19:28:01 -0500

    archivos  de practicas actualizados

---
+++ Listatareas.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.listatareas;
+
+/**
+ *
+ * @author diego
+ */
+public class Listatareas {
+
+    public static void main(String[] args) {
+        int Tareas;
+        String Tarea1 = "Estudiar para el examen";
+        String Tarea2 = "Hacer investigacion del tema programacion";
+        String Tarea3 = "Leer un libro";
+        String Tarea4 = "Estudiar para una expocicion";
+        String Tarea5 = "Realizar los ejercicios de programacion";
+        
+        System.out.println("Primera Tarea;"+Tarea1+ "Segunda Tarea;"+Tarea2+ "Tercer Tarea;"+Tarea3+ "Cuarta Tarea;"+Tarea4+ "Quinta Tarea;"+Tarea5);
+    
+    }
+}
---
+++ Practica2areanombre.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica2areanombre;
+
+/**
+ *
+ * @author diego
+ */
+public class Practica2areanombre {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int Base = 10;
+        int altura = 3;
+        int areaRectangulo = Base*altura;
+        System.out.println("El area del rectangulo es;"+areaRectangulo);
+        String nombre = "Diego Isaac Padua Ortega";
+        System.out.println("El nombre es;"+nombre);
+    }
+}
